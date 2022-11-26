27c3e0c7aa939b0f776b3c61dc789292a38c015d
Nitiwat1 <nitiwat9989@gmail.com>
2022-11-26 11:27:25 +0700

    workshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('Hello kitty');
+  res.status(200).json({
+    fullname:'Nitiwat Phomemarasee'
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  // res.send('Hello kitty');
+  res.status(200).json({
+    fullname:'Nitiwat Phomemarasee',
+    nickname:'Boss',
+    hobby:'sleep',
+    gitusername:'nitiwat1'
+  })
 });
 
 module.exports = router;
