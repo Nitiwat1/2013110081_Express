beb2bc563587772b8119f69146bd88c7944e5ca0
Nitiwat1 <nitiwat9989@gmail.com>
2022-12-10 14:00:10 +0700

    Get All Staff

--- models/company.js
+++ models/company.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const companySchema = new Schema({
-    name : String,
+    name: String,
     address: {
-        province : String
+        province: String
     }
-  },{collection: "setting"});
+}, { collection: "setting" });
 
-  const company = mongoose.model("Companys",companySchema)
+const company = mongoose.model("Companys", companySchema)
 
-  module.exports = company
+module.exports = company
