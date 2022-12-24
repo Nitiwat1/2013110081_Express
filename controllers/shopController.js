1919a28433d35e8f445878e2b20f7d0869093f55
Nitiwat1 <nitiwat9989@gmail.com>
2022-12-24 10:53:01 +0700

    Workshop B

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,9 +1,26 @@
 const Shop = require("../models/shop");
+const Menu = require("../models/menu");
 
 exports.Shop = async (req, res, next) => {
-  const shop = await Shop.find().sort({ _id: -1 });
+    const shops = await Shop.find().select('name photo location').sort({ _id: -1 });
 
-  res.status(200).json({
-    data: shop,
-  });
+
+    const shopWithPhotoDomain = await shops.map((shop, index) => {
+        return {
+            id: shop._id,
+            name: shop.name,
+            photo: 'http://localhost:3000/images/' + shop.photo,
+            location: shop.location
+        }
+    })
+
 };
+exports.menu = async (req , res) => {
+    const shop = await Menu.find()
+      .select('name price')
+      .populate('shop', 'name')
+      .sort({ _id: -1 });
+    res.send({
+      data: shop,
+    });
+  }
